11f05afd1164947bd38e103345ec66d502410696
shadin shalaata <shadinshalaata@shadins-MacBook-Pro.local>
2024-12-04 13:33:49 +0200

    Done MyString

--- MyString.java
+++ MyString.java
@@ -19,12 +19,27 @@ public class MyString {
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
         // Replace the following statement with your code
-        return null;
+        char rep;
+        for(int i=0;i<str.length();i++)
+        {
+            rep=Character.toLowerCase(str.charAt(i));
+            str=str.substring(0, i)+rep+str.substring(i+1);
+
+        }
+        return str;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
         // Replace the following statement with your code
+       
+        if(str1.length()<str2.length()){return false;}
+        for(int i=0;i<str1.length()&&str2.length()+i<=str1.length();i++)
+        {
+       
+            if(str2.equals(str1.substring(i, str2.length()+i)))
+            {return true;}
+        }
         return false;
     }
 }
